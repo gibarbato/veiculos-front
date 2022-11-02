9c7dd4ad53bd46eb1de8a74cc52fbb0b4150bbc2
gilberto barbato <gibarbato@gmail.com>
2022-11-02 08:07:21 -0300

    atualizando pastas front

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import api from './server/api';
 
 function App() {
+  const [novaPlaca, setNovaPlaca] = useState();
+  const [novaMarca, setNovaMarca] = useState();
+  const [novoModelo, setNovoModelo] = useState();
+  const [novoAno, setNovoAno] = useState();
+  const [veiculos, setVeiculos] = useState([{
+    "placa": "qwe1234",
+    "marca": "chery",
+    "modelo": "celer",
+    "ano": 2015
+  },
+  {
+    "placa": "asd1234",
+    "marca": "palio",
+    "modelo": "uno",
+    "ano": 2000
+  },
+  ])
+
+  function carregarVeiculos() {
+    api.get('/veiculos')
+      .then((response) => {
+        setVeiculos(response.data);
+      })
+  }
+
+  function cadastrarVeiculo() {
+    api.post('/veiculos', {
+      placa: novaPlaca,
+      marca : novaMarca,
+      modelo: novoModelo,
+      ano: novoAno,
+    })
+    .then((response)=>{
+      setVeiculos(response.data);
+    })
+  }
+
+  useEffect(() => {
+    carregarVeiculos();
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <div>
+          <input value={novaPlaca} onChange={(e) => { setNovaPlaca(e.target.value) }} placeholder='Placa do Veículo'></input>
+        </div>
+        <div>
+          <input value={novaMarca} onChange={(e) => { setNovaMarca(e.target.value) }} placeholder='Marca do Veículo'></input>
+        </div>
+        <div>
+          <input value={novoModelo} onChange={(e) => { setNovoModelo(e.target.value) }} placeholder='Modelo do Veículo'></input>
+        </div>
+        <div>
+          <input value={novoAno} onChange={(e) => { setNovoAno(e.target.value) }} placeholder='Ano do Veículo'></input>
+        </div>
+        <button onClick={cadastrarVeiculo}>Cadastrar Veículos</button>
+      </div>
+
+      <table>
+        <tr>
+          <th>Placa</th><th>Marca</th><th>Modelo</th><th>Ano</th>
+        </tr>
+        {
+          veiculos.map((veiculo) => {
+            return (
+              <tr>
+                <td>{veiculo.placa}</td><td>{veiculo.marca}</td><td>{veiculo.modelo}</td><td>{veiculo.ano}</td>
+              </tr>
+            )
+          })
+        }
+      </table>
+    </>
   );
 }
 
